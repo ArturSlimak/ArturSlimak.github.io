e735768fb7c4afa29d168c7dc6ebfff22c88f5de
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-06 14:02:55 +0100

    header fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,26 +8,26 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg fixed-top bg-transparent">
       <div className="container-fluid">
         <div className="collapse navbar-collapse">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item me-1">
               <Link className="nav-link active" to="/">
                 Main
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item me-1">
               <Link className="nav-link active" to="/projects">
                 Projects
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item me-1">
               <Link className="nav-link active" to="/reachmeout">
                 Reach me out
               </Link>
             </li>
           </ul>
-          <div className="d-flex me-2">
+          <div className="d-flex ms-2 me-2">
             <div className="nav-item">
-              <a className="github-link" href="https://github.com/ArturSlimac">
+              <a className="github-link" href="https://github.com/ArturSlimak">
                 <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
               </a>
             </div>
