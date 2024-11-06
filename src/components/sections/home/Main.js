8815dc5118462bd423e1eb590313c358fef89795
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-06 15:30:51 +0100

    scroll arrow

--- src/components/sections/home/Main.js
+++ src/components/sections/home/Main.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { FullpageSection } from "@ap.cx/react-fullpage"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
 const Main = () => {
   return (
-    <FullpageSection className="d-flex align-items-center justify-content-center vh-100 yellowBg">
+    <FullpageSection className="d-flex align-items-center justify-content-center vh-100 yellowBg position-relative">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
@@ -12,6 +14,9 @@ const Main = () => {
               languages
             </h4>
           </div>
+        </div>
+        <div className="position-absolute bottom-0 start-50 translate-middle-x mb-4">
+          <FontAwesomeIcon icon={faChevronDown} className="arrow-icon fs-1 " />
         </div>
       </div>
     </FullpageSection>
