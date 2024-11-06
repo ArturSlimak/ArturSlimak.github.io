52ea728c9545a4cc3aa87a9f0f89e20e785e0b65
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-06 16:14:13 +0100

    burger button for mob dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,93 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
 import destinations from "../assets/destinations"
 
 const Header = () => {
   const location = useLocation()
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
-    <nav className="navbar navbar-expand-lg fixed-top bg-transparent">
+    <nav className="navbar fixed-top bg-transparent">
       <div className="container-fluid">
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item me-1">
-              <Link
-                className={`nav-link ${
-                  location.pathname === destinations.main ? "active" : ""
-                }`}
-                to={destinations.main}
-              >
-                Main
-              </Link>
-            </li>
-            <li className="nav-item me-1">
-              <Link
-                className={`nav-link ${
-                  location.pathname === destinations.projects ? "active" : ""
-                }`}
-                to={destinations.projects}
-              >
-                Projects
-              </Link>
-            </li>
-            <li className="nav-item me-1">
-              <Link
-                className={`nav-link ${
-                  location.pathname === destinations.reachmeout ? "active" : ""
-                }`}
-                to={destinations.reachmeout}
-              >
-                Reach me out
-              </Link>
-            </li>
-          </ul>
-          <div className="d-flex ms-2 me-2">
-            <div className="nav-item">
-              <a
-                className="github-link"
-                href="https://github.com/ArturSlimak"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
-              </a>
+        {windowWidth >= 992 ? (
+          // Full navbar for large screens (≥ 992px)
+          <div className="navbar-expand-lg ms-auto">
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ">
+                <li className="nav-item me-1">
+                  <Link
+                    className={`nav-link ${
+                      location.pathname === destinations.main ? "active" : ""
+                    }`}
+                    to={destinations.main}
+                  >
+                    Main
+                  </Link>
+                </li>
+                <li className="nav-item me-1">
+                  <Link
+                    className={`nav-link ${
+                      location.pathname === destinations.projects
+                        ? "active"
+                        : ""
+                    }`}
+                    to={destinations.projects}
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li className="nav-item me-1">
+                  <Link
+                    className={`nav-link ${
+                      location.pathname === destinations.reachmeout
+                        ? "active"
+                        : ""
+                    }`}
+                    to={destinations.reachmeout}
+                  >
+                    Reach me out
+                  </Link>
+                </li>
+              </ul>
+              <div className="d-flex ms-2 me-2">
+                <div className="nav-item">
+                  <a
+                    className="github-link"
+                    href="https://github.com/ArturSlimak"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          // Burger button for small screens (< 992px)
+
+          <div className="ms-auto">
+            <button
+              style={{
+                background: "none",
+                border: "none",
+                cursor: "pointer",
+              }}
+            >
+              <FontAwesomeIcon icon={faBars} className="fa-2xl icons" />
+            </button>
+          </div>
+        )}
       </div>
     </nav>
   )
