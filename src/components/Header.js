1e931836c7e3ed00c245aed835e3c391780892d1
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-06 18:49:17 +0100

    nav slider for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 import destinations from "../assets/destinations"
 
 const Header = () => {
   const location = useLocation()
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const lgScreenBreakPoint = 992
+  const [isSliderOpen, setIsSliderOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    setIsSliderOpen(!isSliderOpen)
+  }
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth)
@@ -17,78 +23,133 @@ const Header = () => {
   }, [])
 
   return (
-    <nav className="navbar fixed-top bg-transparent">
-      <div className="container-fluid">
-        {windowWidth >= 992 ? (
-          // Full navbar for large screens (≥ 992px)
-          <div className="navbar-expand-lg ms-auto">
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ">
-                <li className="nav-item me-1">
-                  <Link
-                    className={`nav-link ${
-                      location.pathname === destinations.main ? "active" : ""
-                    }`}
-                    to={destinations.main}
-                  >
-                    Main
-                  </Link>
-                </li>
-                <li className="nav-item me-1">
-                  <Link
-                    className={`nav-link ${
-                      location.pathname === destinations.projects
-                        ? "active"
-                        : ""
-                    }`}
-                    to={destinations.projects}
-                  >
-                    Projects
-                  </Link>
-                </li>
-                <li className="nav-item me-1">
-                  <Link
-                    className={`nav-link ${
-                      location.pathname === destinations.reachmeout
-                        ? "active"
-                        : ""
-                    }`}
-                    to={destinations.reachmeout}
-                  >
-                    Reach me out
-                  </Link>
-                </li>
-              </ul>
-              <div className="d-flex ms-2 me-2">
-                <div className="nav-item">
-                  <a
-                    className="github-link"
-                    href="https://github.com/ArturSlimak"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
-                  </a>
+    <>
+      {windowWidth >= lgScreenBreakPoint ? (
+        <nav className="navbar fixed-top bg-transparent">
+          <div className="container-fluid">
+            <div className="navbar-expand-lg ms-auto">
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item me-1">
+                    <Link
+                      className={`nav-link ${
+                        location.pathname === destinations.main ? "active" : ""
+                      }`}
+                      to={destinations.main}
+                    >
+                      Main
+                    </Link>
+                  </li>
+                  <li className="nav-item me-1">
+                    <Link
+                      className={`nav-link ${
+                        location.pathname === destinations.projects
+                          ? "active"
+                          : ""
+                      }`}
+                      to={destinations.projects}
+                    >
+                      Projects
+                    </Link>
+                  </li>
+                  <li className="nav-item me-1">
+                    <Link
+                      className={`nav-link ${
+                        location.pathname === destinations.reachmeout
+                          ? "active"
+                          : ""
+                      }`}
+                      to={destinations.reachmeout}
+                    >
+                      Reach me out
+                    </Link>
+                  </li>
+                </ul>
+                <div className="d-flex ms-2 me-2">
+                  <div className="nav-item">
+                    <a
+                      className="github-link"
+                      href="https://github.com/ArturSlimak"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FontAwesomeIcon
+                        icon={faGithub}
+                        className="fa-2xl icons"
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        ) : (
-          // Burger button for small screens (< 992px)
-          <div className="ms-auto mt-1">
-            <button
-              style={{
-                background: "none",
-                border: "none",
-                cursor: "pointer",
-              }}
-            >
-              <FontAwesomeIcon icon={faBars} className="fa-2xl icons" />
-            </button>
+        </nav>
+      ) : (
+        <nav className="navbar fixed-top bg-transparent">
+          <div className="container-fluid">
+            <div className="ms-auto mt-3">
+              <button onClick={toggleDropdown} className="toggleBtn">
+                <FontAwesomeIcon
+                  icon={isSliderOpen ? faTimes : faBars}
+                  className="fa-2xl mobIcons"
+                />
+              </button>
+            </div>
           </div>
-        )}
-      </div>
-    </nav>
+          {/* Add the 'open' class conditionally based on `isSliderOpen` */}
+          <div className={`mobileNav ${isSliderOpen ? "open" : ""}`}>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === destinations.main ? "active" : ""
+                  }`}
+                  to={destinations.main}
+                  onClick={toggleDropdown}
+                >
+                  Main
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === destinations.projects ? "active" : ""
+                  }`}
+                  to={destinations.projects}
+                  onClick={toggleDropdown}
+                >
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === destinations.reachmeout
+                      ? "active"
+                      : ""
+                  }`}
+                  to={destinations.reachmeout}
+                  onClick={toggleDropdown}
+                >
+                  Reach me out
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="github-link"
+                  href="https://github.com/ArturSlimak"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={toggleDropdown}
+                >
+                  <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
+    </>
   )
 }
 
