b0ed25ae87de83a55216cb5d54ad9696de6186d6
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-06 13:25:53 +0100

    github icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { Link, useLocation } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const Header = () => {
   return (
@@ -23,6 +25,13 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+          <div className="d-flex me-2">
+            <div className="nav-item">
+              <a className="github-link" href="https://github.com/ArturSlimac">
+                <FontAwesomeIcon icon={faGithub} className="fa-2xl icons" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
